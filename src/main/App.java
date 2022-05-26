22f481b88b9f1719f9fe0d0f02aa6abfadaa2a9e
Larissa Guder <lariguder10@gmail.com>
2022-05-25 21:34:52 -0300

    feat: add experiments para lock free list

--- src/main/App.java
+++ src/main/App.java
@@ -3,67 +3,73 @@ package main;
 import main.experiments.CoarseListExperiment;
 import main.experiments.FineListExperiment;
 import main.experiments.LazyListExperiment;
+import main.experiments.LockFreeListExperiment;
 import main.experiments.OptimisticListExperiment;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        CoarseListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd, probabilidadeContains,
-                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
-        FineListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd, probabilidadeContains,
-                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
-        LazyListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd, probabilidadeContains,
-                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
-        OptimisticListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd, probabilidadeContains,
-        valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
-    }
+        public static void main(String[] args) throws Exception {
+                CoarseListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd,
+                                probabilidadeContains,
+                                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
+                FineListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd, probabilidadeContains,
+                                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
+                LazyListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd, probabilidadeContains,
+                                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
+                OptimisticListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd,
+                                probabilidadeContains,
+                                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
+                LockFreeListExperiment.runExperiment(numeroThreads, tempoExecucao, probabilidadeAdd,
+                                probabilidadeContains,
+                                valorMinimo, valorMaximo, tamanhoPopulacaoInicial, warmup);
+        }
 
-    // Setup básico. Lista e um conjunto de threads executoras. Cada thread
-    // executora fica em loop
-    // onde sorteia um tipo de operação (veja interface) e invoca o método
-    // correspondente.
+        // Setup básico. Lista e um conjunto de threads executoras. Cada thread
+        // executora fica em loop
+        // onde sorteia um tipo de operação (veja interface) e invoca o método
+        // correspondente.
 
-    // Parametrização das threads:
-    // 1 - número de threads
-    static int numeroThreads = 30;
-    // 2 - acabar threads por tempo (configura) - main para de contabilizar depois
-    // de um tempo e interrompe todas as threads.
-    // Controle externo (main?) às threads que fazem acesso, que executa o que
-    // segue:
-    // - lançar threads
-    // - passar tempo de aquecimento
-    // - inciar contabilizacao
-    // - passar tempo de experimento (medicao)
-    // - parar threads
-    static int tempoExecucao = 60;
+        // Parametrização das threads:
+        // 1 - número de threads
+        static int numeroThreads = 6;
+        // 2 - acabar threads por tempo (configura) - main para de contabilizar depois
+        // de um tempo e interrompe todas as threads.
+        // Controle externo (main?) às threads que fazem acesso, que executa o que
+        // segue:
+        // - lançar threads
+        // - passar tempo de aquecimento
+        // - inciar contabilizacao
+        // - passar tempo de experimento (medicao)
+        // - parar threads
+        static int tempoExecucao = 10;
 
-    // 3 - distribuição de probabilidade para operações add, delete, contains. ->>
-    // prob add > prob delete
-    static int probabilidadeAdd = 40, probabilidadeDelete = 30, probabilidadeContains = 30;
+        // 3 - distribuição de probabilidade para operações add, delete, contains. ->>
+        // prob add > prob delete
+        static int probabilidadeAdd = 40, probabilidadeDelete = 30, probabilidadeContains = 30;
 
-    // 4 - o espaço de valores para sortear operações --> limita tamanho máximo da
-    // lista (nao duplica valor dentro)
-    static int valorMinimo = 1;
+        // 4 - o espaço de valores para sortear operações --> limita tamanho máximo da
+        // lista (nao duplica valor dentro)
+        static int valorMinimo = 1;
 
-    static int valorMaximo = 100000;
+        static int valorMaximo = 5000;
 
-    // 5 - medir tamanho médio da lista
-    static volatile int countAdd = 0, countContains = 0, countRemove = 0;
-    // Parametrização da lista (questões envolvendo a lista)
+        // 5 - medir tamanho médio da lista
+        static volatile int countAdd = 0, countContains = 0, countRemove = 0;
+        // Parametrização da lista (questões envolvendo a lista)
 
-    // 1 - população inicial - gerar uma lista inicialmente populada - para diminuir
-    // tempo de warm-up (Ex.: 100k, 200k, 300k elementos)
-    static int tamanhoPopulacaoInicial = 100000;
-    // 2 - limitar tamanho da lista para nao ter crescimento indefinido, ou entao
-    // limitar o espaço de chaves tal que ao inserir uma chave que já está presente
-    // nao vai aumentar a lista. O aumento da lista vai gerar tempos crescentes para
-    // as operacoes. Precisamos de uma lista de tamanho estacionário.
+        // 1 - população inicial - gerar uma lista inicialmente populada - para diminuir
+        // tempo de warm-up (Ex.: 100k, 200k, 300k elementos)
+        static int tamanhoPopulacaoInicial = 5000;
+        // 2 - limitar tamanho da lista para nao ter crescimento indefinido, ou entao
+        // limitar o espaço de chaves tal que ao inserir uma chave que já está presente
+        // nao vai aumentar a lista. O aumento da lista vai gerar tempos crescentes para
+        // as operacoes. Precisamos de uma lista de tamanho estacionário.
 
-    // 3 - Tempo de execução e warm-up. Definir um tempo inicial onde medidas não
-    // são tomadas pois a lista está ainda variando
-    // rapidamente de tamanho e assim de tempo de execucao, influenciando na média.
-    // Sugere-se inicialmente 10 s de warmup e 1 min de experimento. Entretanto
-    // estes valores devem ser investigados empiricamente.
-    // Ou seja, veja se os resultados são sensíveis a estes parâmetros.
-    static int warmup = 10000;
+        // 3 - Tempo de execução e warm-up. Definir um tempo inicial onde medidas não
+        // são tomadas pois a lista está ainda variando
+        // rapidamente de tamanho e assim de tempo de execucao, influenciando na média.
+        // Sugere-se inicialmente 10 s de warmup e 1 min de experimento. Entretanto
+        // estes valores devem ser investigados empiricamente.
+        // Ou seja, veja se os resultados são sensíveis a estes parâmetros.
+        static int warmup = 10;
 
 }
---
+++ src/main/experiments/LockFreeListExperiment.java
@@ -0,0 +1,109 @@
+package main.experiments;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import main.experiments.util.Random;
+import main.lists.LockFreeList;
+
+public class LockFreeListExperiment {
+    static boolean running = true;
+    static boolean count = false;
+
+    static volatile int countAdd = 0, countContains = 0, countRemove = 0;
+
+    static LockFreeList<Integer> lockFreeList = new LockFreeList<>();
+
+    static Thread thread(int n, int probabilidadeAdd,
+            int probabilidadeContains, int valorMinimo, int valorMaximo) {
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while (running) {
+                    if (Random.getRandomOperation(probabilidadeAdd, probabilidadeContains) == 3) {
+                        lockFreeList.add(Random.getRandom(valorMinimo, valorMaximo));
+                        if (count) {
+                            countAdd++;
+                        }
+
+                    } else if (Random.getRandomOperation(probabilidadeAdd, probabilidadeContains) == 2) {
+                        lockFreeList.contains(Random.getRandom(valorMinimo, valorMaximo));
+                        if (count)
+                            countContains++;
+                    } else {
+                        lockFreeList.remove(Random.getRandom(valorMinimo, valorMaximo));
+                        if (count)
+                            countRemove++;
+                    }
+                }
+            }
+
+        };
+        t.start();
+        return t;
+    }
+
+    // Consolidado
+    static void testThreads(int TH, int tempoExecucao, int probabilidadeAdd,
+            int probabilidadeContains, int valorMinimo, int valorMaximo) {
+        Thread[] threads = new Thread[TH];
+        Thread t;
+        for (int i = 0; i < TH; i++) {
+            t = thread(i, probabilidadeAdd,
+                    probabilidadeContains, valorMinimo, valorMaximo);
+            threads[i] = t;
+        }
+
+        try {
+            // WarmUp
+            Thread.sleep(10000);
+            count = true;
+
+            Thread.sleep(tempoExecucao * 1000);
+            running = false;
+            for (int i = 0; i < TH; i++) {
+                threads[i].join();
+            }
+        } catch (InterruptedException e) {
+            System.err.println(e.toString());
+
+        } finally {
+        }
+    }
+
+    public static void runExperiment(int numeroThreads, int tempoExecucao, int probabilidadeAdd,
+            int probabilidadeContains, int valorMinimo, int valorMaximo, int tamanhoPopulacaoInicial,
+            int warmup) {
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter("LockFreeListExperiment.csv", "UTF-8");
+            writer.println("threads,add,contains,remove,population");
+            for (int i = 2; i <= numeroThreads; i = i + 2) {
+                lockFreeList = new LockFreeList<>();
+                countRemove = 0;
+                countContains = 0;
+                countAdd = 0;
+
+                for (int k = 0; k < tamanhoPopulacaoInicial; k++) {
+                    lockFreeList.add(k);
+                }
+
+                running = true;
+                count = false;
+
+                testThreads(i, tempoExecucao, probabilidadeAdd, probabilidadeContains, valorMinimo, valorMaximo);
+                writer.print(i + ",");
+                writer.print(countAdd / tempoExecucao + ",");
+                writer.print(countContains / tempoExecucao + ",");
+                writer.print(countRemove / tempoExecucao + ",");
+                writer.println(lockFreeList.count());
+            }
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
